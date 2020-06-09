7833a95baaedbea76535d875d27d548db024499a
Mikael Korpinen <mikael.korpinen@gmail.com>
2020-06-10 00:10:30 +0300

    First commit

---
+++ Assets/GeometricVision/GeometryVisionEye.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Collections.Generic;
+using GeometricVision;
+using GeometricVision.Jobs;
+using GeometricVision.Utilities;
+using Unity.Jobs;
+using UnityEngine;
+using Plane = UnityEngine.Plane;
+
+public enum GeometryType
+{
+    all,
+    Vertices,
+    Edges,
+    Objects_,
+    Colliders
+}
+
+[Serializable]
+public class VisionTarget
+{
+    public bool onOff = true;
+    public GeometryType type;
+
+    [SerializeField, Layer] private int targetLayer = 31;
+
+    public int TargetLayer
+    {
+        get { return targetLayer; }
+        set { targetLayer = value; }
+    }
+
+    public VisionTarget(GeometryType geoType, int layerIndex)
+    {
+        type = geoType;
+        targetLayer = layerIndex;
+    }
+}
+
+/// <summary>
+/// Class that is responsible for seeing objects and geometry.
+/// It checks, if object is inside visibility area and filters out unwanted objects and geometry.
+/// 
+/// </summary>
+public class GeometryVisionEye : MonoBehaviour
+{
+    [SerializeField] private bool debugMode;
+    public VisionTarget[] geometryTypes;
+
+    [SerializeField] private bool hideEdgesOutsideFieldOfView = true;
+    [SerializeField] private bool showSeenEdges = true;
+    [SerializeField] private float fieldOfView = 25f;
+    [SerializeField] private int lastCount = 0;
+    [SerializeField] private List<GeometryDataModels.GeoInfo> _seenGeoInfos = new List<GeometryDataModels.GeoInfo>();
+    [SerializeField] private IGeoBrain _controllerBrain;
+
+    public List<GeometryDataModels.GeoInfo> SeenGeoInfos
+    {
+        get { return _seenGeoInfos; }
+        set { _seenGeoInfos = value; }
+    }
+
+    public struct filterPlane
+    {
+        public Vector3 positiom;
+        public Quaternion rotation;
+        public Vector3 direction;
+        public Transform rootObject;
+    }
+
+    private Camera camera;
+    public Plane[] _planes = new Plane[6];
+    [SerializeField] public HashSet<Transform> SeenObjects;
+    private EyeDebugger _debugger;
+    private bool _addedByFactory;
+
+    public Plane[] Planes
+    {
+        get { return _planes; }
+        set { _planes = value; }
+    }
+
+    public Camera Camera1
+    {
+        get { return camera; }
+        set { camera = value; }
+    }
+
+    public IGeoBrain ControllerBrain
+    {
+        get { return _controllerBrain; }
+        set { _controllerBrain = value; }
+    }
+
+    public GeometryVisionHead Head { get; set; }
+
+
+    private void Awake()
+    {
+        if (gameObject.GetComponent<Camera>() != null)
+        {
+            Camera1 = gameObject.GetComponent<Camera>();
+        }
+
+        _seenGeoInfos = new List<GeometryDataModels.GeoInfo>();
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (gameObject.GetComponent<Camera>() == null)
+        {
+            gameObject.AddComponent<Camera>();
+            Camera1 = gameObject.GetComponent<Camera>();
+        }
+
+
+        ControllerBrain = getControllerFromGeometryManager();
+
+        _debugger = new EyeDebugger();
+        SeenObjects = new HashSet<Transform>();
+        Camera1.enabled = false;
+        _debugger.Planes = RegenerateVisionArea(fieldOfView, _planes);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _planes = RegenerateVisionArea(fieldOfView, _planes);
+        UpdateVisibility(SeenObjects, _seenGeoInfos);
+        Debug();
+    }
+
+    /// <summary>
+    /// Updates visibility of the objects in the eye and brain/manager
+    /// </summary>
+    /// <param name="seenObjects"></param>
+    /// <param name="seenGeoInfos"></param>
+    private void UpdateVisibility(HashSet<Transform> seenObjects, List<GeometryDataModels.GeoInfo> seenGeoInfos)
+    {
+        SeenObjects = UpdateObjectVisibility(ControllerBrain.getAllObjects(), seenObjects);
+        SeenGeoInfos = UpdateGeometryVisibility(_planes, ControllerBrain.GeoInfos(), seenGeoInfos);
+    }
+
+    /// <summary>
+    /// Update gameobject visibility. Object that do not have geometry in it
+    /// </summary>
+    private HashSet<Transform> UpdateObjectVisibility(List<Transform> listToCheck, HashSet<Transform> seenTransforms)
+    {
+        seenTransforms = new HashSet<Transform>();
+
+        seenTransforms = GetObjectsInsideFrustum(seenTransforms, listToCheck);
+
+
+        return seenTransforms;
+    }
+
+    /// <summary>
+    /// Hides Edges, vertices, geometryObject outside th frustum
+    /// </summary>
+    /// <param name="planes"></param>
+    /// <param name="allGeoInfos"></param>
+    private List<GeometryDataModels.GeoInfo> UpdateGeometryVisibility(Plane[] planes,
+        List<GeometryDataModels.GeoInfo> allGeoInfos, List<GeometryDataModels.GeoInfo> seenGeometry)
+    {
+        int geoCount = allGeoInfos.Count;
+        seenGeometry = new List<GeometryDataModels.GeoInfo>();
+
+        UpdateSeenGeometryObjects(allGeoInfos, seenGeometry, geoCount);
+
+        foreach (var geometryType in geometryTypes)
+        {
+            if (geometryType.type == GeometryType.Edges)
+            {
+                MeshUtilities.UpdateEdgesVisibilityParallel(planes, seenGeometry);
+            }
+        }
+
+        return seenGeometry;
+    }
+
+    /// <summary>
+    /// Updates object collection containing geometry and data related to seen object. Usage is to internally update seen geometry objects by checking objects renderer bounds
+    /// against eyes/cameras frustum
+    /// </summary>
+    /// <param name="allGeoInfos"></param>
+    /// <param name="seenGeometry"></param>
+    /// <param name="geoCount"></param>
+    private void UpdateSeenGeometryObjects(List<GeometryDataModels.GeoInfo> allGeoInfos,
+        List<GeometryDataModels.GeoInfo> seenGeometry, int geoCount)
+    {
+        if (geometryIsTargeted())
+        {
+            for (var i = 0; i < geoCount; i++)
+            {
+                var geInfo = allGeoInfos[i];
+
+                if (GeometryUtility.TestPlanesAABB(_planes, allGeoInfos[i].renderer.bounds) &&
+                    hideEdgesOutsideFieldOfView)
+                {
+                    seenGeometry.Add(geInfo);
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Check if user has selected mesh geometry as target for the operation
+    /// </summary>
+    /// <returns></returns>
+    private bool geometryIsTargeted()
+    {
+        bool found = false;
+        foreach (var visionTarget in geometryTypes)
+        {
+            if (visionTarget.type == GeometryType.Edges || visionTarget.type == GeometryType.Vertices)
+            {
+                found = true;
+            }
+        }
+
+        return found;
+    }
+
+    public IGeoBrain getControllerFromGeometryManager()
+    {
+        var head = FindObjectOfType<GeometryVisionHead>();
+        if (head == null)
+        {
+            var factory = new GeometryVisionFactory();
+            var headObject = factory.CreateGeometryVision(new Vector3(0f, 0f, 0f), Quaternion.identity, 25, this,
+                GeometryType.Edges, 0);
+            return headObject.GetComponent<GeometryVisionBrain>();
+        }
+
+        return head.GetComponent<GeometryVisionBrain>();
+    }
+
+    /// <summary>
+    /// When the camera is moved, rotated or both the frustum planes that
+    /// hold the system together needs to be refreshes/regenerated
+    /// </summary>
+    /// <param name="fieldOfView"></param>
+    /// <returns>Plane[]</returns>
+    /// <remarks>Faster way to get the current situation for planes might be to store planes into an object and move them with the eye</remarks>
+    private Plane[] RegenerateVisionArea(float fieldOfView, Plane[] planes)
+    {
+        Camera1.enabled = true;
+        Camera1.fieldOfView = fieldOfView;
+        planes = GeometryUtility.CalculateFrustumPlanes(Camera1);
+        Camera1.enabled = false;
+        return planes;
+    }
+
+    /// <summary>
+    /// When the camera is moved, rotated or both the frustum planes that
+    /// hold the system together needs to be refreshes/regenerated
+    /// </summary>
+    /// <param name="fieldOfView"></param>
+    /// <returns>void</returns>
+    /// <remarks>Faster way to get the current situation for planes might be to store planes into an object and move them with the eye</remarks>
+    public void RegenerateVisionArea(float fieldOfView)
+    {
+        Camera1.enabled = true;
+        Camera1.fieldOfView = fieldOfView;
+        _planes = GeometryUtility.CalculateFrustumPlanes(Camera1);
+        Camera1.enabled = false;
+    }
+
+    private HashSet<Transform> GetObjectsInsideFrustum(HashSet<Transform> seenTransforms, List<Transform> allTransforms)
+    {
+        foreach (var transform in allTransforms)
+        {
+            if (MeshUtilities.IsInsideFrustum(transform.position, _planes))
+            {
+                seenTransforms.Add(transform);
+                lastCount = seenTransforms.Count;
+            }
+        }
+
+        return seenTransforms;
+    }
+
+
+    private void RemoveAt(ref Renderer[] renderer, int indexToRemove)
+    {
+        for (int i = indexToRemove; i < renderer.Length - 1; i++)
+        {
+            renderer[i] = renderer[i + 1];
+        }
+
+        Destroy(gameObject.GetComponent<Camera>());
+
+        Array.Resize(ref renderer, renderer.Length - 1);
+    }
+
+    private void Debug()
+    {
+        if (debugMode)
+        {
+            _debugger.Debug(Camera1, SeenGeoInfos, true);
+        }
+    }
+}
---
+++ Assets/Tests/GeoUtilitiesTests.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using GeometricVision;
+using GeometricVision.Utilities;
+using NUnit.Framework;
+using Unity.PerformanceTesting;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+using static GeometryDataModels.Boolean;
+using Object = UnityEngine.Object;
+
+namespace Tests
+{
+    [TestFixture]
+    public class GeoUtilitiesTests
+    {
+        private const string version = TestSettings.Version;
+        private Tuple<GeometryVisionFactory, EditorBuildSettingsScene[]> factoryAndOriginalScenes;
+        
+        [TearDown]
+        public void TearDown()
+        {
+            //Put original scenes back to build settings
+            EditorBuildSettings.scenes = factoryAndOriginalScenes.Item2;
+        }
+
+        [UnityTest, Performance, Version(version)]
+        [Timeout(TestSettings.defaultPerformanceTests)]
+        public IEnumerator GetEdgesOnCubeReturns18Edges([ValueSource(typeof(TestUtilities), nameof(TestUtilities.GetScenesFromPath))]
+            string scenePath)
+        {
+            int expectedEdgeCount = 12 + 6; //corner edges + triangulated edges
+            factoryAndOriginalScenes = TestUtilities.SetupScene(scenePath);
+            for (int i = 0; i < 50; i++)
+            {
+                yield return null;
+            }
+
+            var testCubes = Object.FindObjectsOfType<GameObject>();
+            yield return null;
+            var testCubeCount = testCubes.Where(tc => tc.name.Contains("Cube")).ToList().Count;
+            GameObject geoVision = factoryAndOriginalScenes.Item1.CreateGeometryVision(new Vector3(0f, 0f, -6f), Quaternion.identity, 25,
+                GeometryType.Edges, 0);
+            var geoEye = geoVision.GetComponent<GeometryVisionEye>();
+            var cube = GameObject.Find("Cube");
+            yield return null;
+            //Since we are not moving the geoVisionObject we don't need to recalculate planes from camera component
+            Assert.AreEqual(expectedEdgeCount,
+                MeshUtilities.GetEdgesFromMesh(cube.GetComponent<Renderer>(), cube.GetComponent<MeshFilter>().mesh)
+                    .Length);
+        }
+
+        [UnityTest, Performance, Version(version)]
+        [Timeout(TestSettings.defaultPerformanceTests)]
+        public IEnumerator NoResultIfNoGeometryTargeted([ValueSource(typeof(TestUtilities), nameof(TestUtilities.GetScenesFromPath))]
+            string scenePath)
+        {
+            factoryAndOriginalScenes = TestUtilities.SetupScene(scenePath);
+
+            for (int i = 0; i < 50; i++)
+            {
+                yield return null;
+            }
+
+            var testCubes = Object.FindObjectsOfType<GameObject>();
+            yield return null;
+            var testCubeCount = testCubes.Where(tc => tc.name.Contains("Cube")).ToList().Count;
+            var expectedEdgeCount = 0;
+            var cube = GameObject.Find("Cube");
+            yield return null;
+
+            cube.transform.position = Vector3.zero;
+            GameObject geoVision = factoryAndOriginalScenes.Item1.CreateGeometryVision(new Vector3(0f, 0f, -6f), Quaternion.identity, 25,
+                GeometryType.Objects_, 0);
+            yield return null;
+            GeometryDataModels.Edge[] edges;
+            var geoEye = geoVision.GetComponent<GeometryVisionEye>();
+
+            /////Put camera at position where it can only see text cube 3d model partially
+            var position = new Vector3(-0.69f, 0.352f, -4.34f);
+            //Need to wait till update loop finishes updating. Most likely issue with slow pc's.
+            yield return null;
+
+            var visibleEdgeCount =
+                GetTestResultsFromPositionMultiThreaded(geoVision, geoEye, cube, out edges, position);
+            Assert.AreEqual(expectedEdgeCount, visibleEdgeCount);
+        }
+
+        [UnityTest, Performance, Version(version)]
+        [Timeout(TestSettings.defaultPerformanceTests)]
+        public IEnumerator GetEdgesOnPartiallyVisibleCubeReturnsRightAmountOfEdges(
+            [ValueSource(typeof(TestUtilities), nameof(TestUtilities.GetScenesFromPath))]
+            string scenePath)
+        {
+            factoryAndOriginalScenes = TestUtilities.SetupScene(scenePath);
+
+            for (int i = 0; i < 50; i++)
+            {
+                yield return null;
+            }
+
+            var testCubes = Object.FindObjectsOfType<GameObject>();
+            yield return null;
+            var testCubeCount = testCubes.Where(tc => tc.name.Contains("Cube")).ToList().Count;
+            var expectedEdgeCount = testCubeCount * 3;
+            var cube = GameObject.Find("Cube");
+            yield return null;
+            cube.transform.position = Vector3.zero;
+            GameObject geoVision = factoryAndOriginalScenes.Item1.CreateGeometryVision(new Vector3(0f, 0f, -6f), Quaternion.identity, 25,
+                GeometryType.Edges, 0);
+            yield return null;
+            GeometryDataModels.Edge[] edges;
+            var geoEye = geoVision.GetComponent<GeometryVisionEye>();
+
+            /////Put camera at position where it can only see text cube 3d model partially
+            var position = new Vector3(-0.69f, 0.352f, -4.34f);
+            //Need to wait till update loop finishes for frustum to update. On windows machines not happen as fast as on Linux for some reason.
+            yield return null;
+
+            var visibleEdgeCount =
+                GetTestResultsFromPositionMultiThreaded(geoVision, geoEye, cube, out edges, position);
+            yield return null;
+            Assert.AreEqual(expectedEdgeCount, visibleEdgeCount);
+        }
+
+        [UnityTest, Performance, Version(version)]
+        [Timeout(TestSettings.defaultPerformanceTests)]
+        public IEnumerator GetEdgesOnPartiallyVisibleCubeReturns3EdgesMultiThreaded(
+            [ValueSource(typeof(TestUtilities), nameof(TestUtilities.GetScenesFromPath))]
+            string scenePath)
+        {
+            factoryAndOriginalScenes = TestUtilities.SetupScene(scenePath);
+
+            for (int i = 0; i < 50; i++)
+            {
+                yield return null;
+            }
+
+            var testCubes = Object.FindObjectsOfType<GameObject>();
+            yield return null;
+            var testCubeCount = testCubes.Where(tc => tc.name.Contains("Cube")).ToList().Count;
+            var expectedEdgeCount = testCubeCount * 3;
+            var cube = GameObject.Find("Cube");
+            yield return null;
+            cube.transform.position = Vector3.zero;
+            GameObject geoVision = factoryAndOriginalScenes.Item1.CreateGeometryVision(new Vector3(0f, 0f, -6f), Quaternion.identity, 25,
+                GeometryType.Edges, 0);
+            yield return null;
+            GeometryDataModels.Edge[] edges;
+            var geoEye = geoVision.GetComponent<GeometryVisionEye>();
+
+            /////Put camera at position where it can only see text cube 3d model partially(3 edges of side of the cube)
+            var position = new Vector3(-0.69f, 0.352f, -4.34f);
+            var visibleEdgeCount =
+                GetTestResultsFromPositionMultiThreaded(geoVision, geoEye, cube, out edges, position);
+            Assert.AreEqual(expectedEdgeCount, visibleEdgeCount);
+        }
+
+        [UnityTest, Performance, Version(version)]
+        [Timeout(TestSettings.defaultPerformanceTests)]
+        public IEnumerator CubeReturnsCorrectEdgesWhenMovingCameraBackAndForth(
+            [ValueSource(typeof(TestUtilities), nameof(TestUtilities.GetScenesFromPath))]
+            string scenePath)
+        {
+            factoryAndOriginalScenes = TestUtilities.SetupScene(scenePath);
+
+            for (int i = 0; i < 50; i++)
+            {
+                yield return null;
+            }
+
+            var testCubes = Object.FindObjectsOfType<GameObject>();
+            yield return null;
+            var testCubeCount = testCubes.Where(tc => tc.name.Contains("Cube")).ToList().Count;
+            var expectedEdgeCount = testCubeCount * 3;
+            var cube = GameObject.Find("Cube");
+            yield return null;
+
+            yield return null;
+            cube.transform.position = Vector3.zero;
+            GameObject geoVision = factoryAndOriginalScenes.Item1.CreateGeometryVision(new Vector3(0f, 0f, -6f), Quaternion.identity, 25,
+                GeometryType.Edges, 0);
+            yield return null;
+            var geoEye = geoVision.GetComponent<GeometryVisionEye>();
+
+            /////Put camera at starting position so it can see the 3d model.
+            GeometryDataModels.Edge[] edges = new GeometryDataModels.Edge[0];
+            var position = new Vector3(-0.69f, 0.352f, -4.34f);
+            var visibleEdgeCount = GetTestResultsFromPosition(geoVision, geoEye, cube, out edges, position);
+            Assert.AreEqual(expectedEdgeCount, visibleEdgeCount);
+
+
+            /////Move the eye so it sees the whole cube
+            expectedEdgeCount = testCubeCount * (12 + 6); //corner edges + triangulated edges
+            position = new Vector3(-0.06f, 0.352f, -12.0f);
+            visibleEdgeCount = GetTestResultsFromPosition(geoVision, geoEye, cube, out edges, position);
+            Assert.AreEqual(expectedEdgeCount, visibleEdgeCount);
+
+            //Move the eye back so it only sees 3 edges
+            expectedEdgeCount = testCubeCount * 3;
+            position = new Vector3(-0.69f, 0.352f, -4.34f);
+            visibleEdgeCount = GetTestResultsFromPosition(geoVision, geoEye, cube, out edges, position);
+
+            Assert.AreEqual(expectedEdgeCount, visibleEdgeCount);
+        }
+
+        private static int GetTestResultsFromPosition(GameObject geoVision, GeometryVisionEye geoEye, GameObject cube,
+            out GeometryDataModels.Edge[] edges, Vector3 position)
+        {
+            edges = new GeometryDataModels.Edge[0];
+            var edgesT = edges;
+            geoVision.transform.position = position;
+            geoEye.RegenerateVisionArea(25);
+            var renderer = cube.GetComponent<Renderer>();
+            Measure.Method(() => { MeshUtilities.UpdateEdgesVisibility(geoEye.Planes, geoEye.SeenGeoInfos); }).Run();
+            var visibleEdgeCount = 0;
+
+            foreach (var geo in geoEye.SeenGeoInfos)
+            {
+                visibleEdgeCount += geo.edges.Where(edge => edge.isVisible == True).ToList().Count;
+            }
+
+            return visibleEdgeCount;
+        }
+
+        private static int GetTestResultsFromPositionMultiThreaded(GameObject geoVision, GeometryVisionEye geoEye,
+            GameObject cube, out GeometryDataModels.Edge[] edges, Vector3 position)
+        {
+            edges = new GeometryDataModels.Edge[0];
+            var edgesT = edges;
+            geoVision.transform.position = position;
+            geoEye.RegenerateVisionArea(25);
+            var renderer = cube.GetComponent<Renderer>();
+
+            Measure.Method(() =>
+            {
+                MeshUtilities.UpdateEdgesVisibilityParallel(geoEye.Planes, geoEye.SeenGeoInfos);
+                ///TODO:check visibility for every object
+            }).Run();
+
+            return CalculateAmountOfVisibleEdges(geoEye);
+        }
+
+        private static int CalculateAmountOfVisibleEdges(GeometryVisionEye geoEye)
+        {
+            var visibleEdgeCount = 0;
+            foreach (var geo in geoEye.SeenGeoInfos)
+            {
+                visibleEdgeCount += geo.edges.Where(edge => edge.isVisible == True).ToList().Count;
+            }
+
+            return visibleEdgeCount;
+        }
+    }
+}
